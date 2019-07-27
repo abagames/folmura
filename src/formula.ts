68c10d1d6ab9a27d80213a01d1757ebfcd9decee
ABA Games <cs8k-cyu@asahi-net.or.jp>
2019-07-27 20:30:21 +0900

    Add formula

---
+++ src/formula.ts
@@ -0,0 +1,94 @@
+import { p } from "./main";
+import { Random } from "./random";
+import { range } from "./math";
+
+type Formula = {
+  func: Function;
+  args?: Formula[];
+  value?: number | string;
+};
+
+export function generate(random: Random, depth = 0): Formula {
+  const r = random.get();
+  const dr = depth / 3;
+  if (r < 0.25 * dr) {
+    return {
+      func: constant,
+      value: random.get(0.5, 2) * random.getPlusOrMinus()
+    };
+  } else if (r < 0.5 * dr) {
+    return {
+      func: variable,
+      value: random.select(["t", "t", "i", "x", "y"])
+    };
+  } else {
+    const funcs = [
+      [plus, 2],
+      [minus, 2],
+      [multiple, 2],
+      [divide, 2],
+      [sin, 1],
+      [sin, 1],
+      [cos, 1],
+      [cos, 1],
+      [exp, 1],
+      [pow, 2],
+      [noise, 1]
+    ];
+    const fa = random.select(funcs);
+    const func = fa[0];
+    const args = range(fa[1]).map(() => generate(random, depth + 1));
+    return { func, args };
+  }
+}
+
+export function calc(formula: Formula, variables) {
+  return formula.func(formula, variables);
+}
+
+function plus(formula: Formula, variables) {
+  return calc(formula.args[0], variables) + calc(formula.args[1], variables);
+}
+
+function minus(formula: Formula, variables) {
+  return calc(formula.args[0], variables) - calc(formula.args[1], variables);
+}
+
+function multiple(formula: Formula, variables) {
+  return calc(formula.args[0], variables) * calc(formula.args[1], variables);
+}
+
+function divide(formula: Formula, variables) {
+  return calc(formula.args[0], variables) / calc(formula.args[1], variables);
+}
+
+function sin(formula: Formula, variables) {
+  return p.sin(calc(formula.args[0], variables));
+}
+
+function cos(formula: Formula, variables) {
+  return p.cos(calc(formula.args[0], variables));
+}
+
+function exp(formula: Formula, variables) {
+  return p.exp(calc(formula.args[0], variables));
+}
+
+function pow(formula: Formula, variables) {
+  return p.pow(
+    calc(formula.args[0], variables),
+    calc(formula.args[1], variables)
+  );
+}
+
+function noise(formula: Formula, variables) {
+  return p.noise(calc(formula.args[0], variables));
+}
+
+function constant(formula: Formula) {
+  return formula.value;
+}
+
+function variable(formula: Formula, variables) {
+  return variables[formula.value];
+}
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,44 @@
 import p5 from "p5";
+import * as formula from "./formula";
+import { Random } from "./random";
+import { range, wrap } from "./math";
+import { Vector } from "./vector";
 
-let p: p5;
+export let p: p5;
 
-function setup() {}
+const random = new Random();
+const xFunc = formula.generate(random);
+const yFunc = formula.generate(random);
+const variables: any = {};
+const shapes = range(10).map(() => {
+  return { pos: new Vector() };
+});
+
+let t = 0;
+
+function setup() {
+  p.noStroke();
+  p.background(0);
+}
 
 function draw() {
-  p.clear();
-  p.rect(10, 20, 30, 40);
+  p.fill(0, 20);
+  p.rect(0, 0, p.width, p.height);
+  variables["t"] = t;
+  shapes.forEach((s, i) => {
+    variables["i"] = i;
+    variables["x"] = s.pos.x;
+    variables["y"] = s.pos.y;
+    s.pos.set(formula.calc(xFunc, variables), formula.calc(yFunc, variables));
+    p.fill(200, 100);
+    p.rect(
+      wrap(s.pos.x + p.width / 2, 0, p.width),
+      wrap(s.pos.y + p.height / 2, 0, p.height),
+      10,
+      10
+    );
+  });
+  t++;
 }
 
 new p5((_p: p5) => {
